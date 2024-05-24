fcfa930c0d549b63c932fc879746e330eae14424
Rajeev mishra <113230206+Itsrajeev123@users.noreply.github.com>
2024-05-24 15:02:14 +0530

    Update userRoutes.js

--- userRoutes.js
+++ userRoutes.js
@@ -1,37 +1,41 @@
 const { Router } = require("express");
-const router = Router();
 const User = require("../models/user");
 
+const router = Router();
+
 router.get("/signin", (req, res) => {
-    return res.render("signin");
+  return res.render("signin");
 });
 
 router.get("/signup", (req, res) => {
-    return res.render("signup");
+  return res.render("signup");
 });
 
-router.post("/signin",async(req,res)=>{
-    const {email,password}=req.body;
-    const user=await User.matchPassword(email,password);
+router.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const token = await User.matchPasswordAndGenerateToken(email, password);
 
-    console.log("User",user);
-    return res.redirect("/");
-})
+    return res.cookie("token", token).redirect("/");
+  } catch (error) {
+    return res.render("signin", {
+      error: "Incorrect Email or Password",
+    });
+  }
+});
+
+router.get("/logout", (req, res) => {
+  res.clearCookie("token").redirect("/");
+});
 
 router.post("/signup", async (req, res) => {
-    const { fullname, email, password } = req.body;
-    
-    try {
-        const newUser = await User.create({
-            fullname,
-            email,
-            password,
-        });
-        return res.redirect("/");
-    } catch (error) {
-        console.error("Error creating user:", error);
-        return res.status(500).send("Error creating user. Please try again later.");
-    }
+  const { fullName, email, password } = req.body;
+  await User.create({
+    fullName,
+    email,
+    password,
+  });
+  return res.redirect("/");
 });
 
 module.exports = router;
